3d9b45d6112b441ce9459e388a1b351976861d48
Skyawesome73 <skyawesome73@gmail.com>
2022-12-19 14:02:12 -0600

    Initial Commit

---
+++ ToolbarProject/ToolbarProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToolbarProjectApp.swift
+//  ToolbarProject
+//
+//  Created by Nicholas Puig on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ToolbarProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
